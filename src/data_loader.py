e71341628ee1d3a2f50e0a4de34b9400a94be1bd
Robert Solli <solli@1x-193-157-182-57.uio.no>
2019-01-24 14:59:56 +0100

    running draw model

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import h5py
 
+
 def DataLoader(file_location):
     fileobj = h5py.File(file_location, "r")
     X_t = fileobj["train_features"]
@@ -11,7 +12,8 @@ def DataLoader(file_location):
 
     return X_t, y_t, X_v, y_v
 
+
 if __name__ == "__main__":
 
-    file_location = "/home/solli-comphys/github/VAE-event-classification/data/real/packaged/x-y/proton-carbon-junk-noise.h5"
+    file_location = "~/Documents/github/VAE-event-classification/data/real/packaged/x-y/proton-carbon-junk-noise.h5"
     a = DataLoader(file_location)
