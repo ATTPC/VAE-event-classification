ea7b3ef3410a7a3d563f3e55f8e55824035245f1
Robert Solli <solli.robert@gmail.com>
2019-04-12 12:58:48 +0200

    updated gridsearch

--- scripts/conVAE_test.py
+++ scripts/conVAE_test.py
@@ -3,18 +3,33 @@ sys.path.append("../src")
 
 from convolutional_VAE import ConVae
 import numpy as np
+import keras as ker
+import os
 
 
+print("PID: ", os.getpid())
 input_dim = (128, 128, 1)
-X = np.zeros((500, 128, 128, 1))
+X = np.load("../data/processed/all_0130.npy")
 
-n_layers = 2 
-filter_architecture = [50, 40]
-kernel_arcitecture = [7, 5,]
-strides_architecture = [4, 2]
+n_layers = 4 
+filter_architecture = [20, 40, 10, 5]
+kernel_arcitecture = [7, 5, 3, 2]
+strides_architecture = [4, 2, 2, 1]
 
 latent_dim = 3
 batch_size = 50
+
+earlystop = ker.callbacks.EarlyStopping(
+                    monitor='loss',
+                    min_delta=0.1,
+                    patience=0,
+                    verbose=0,
+                    mode='auto',
+                    restore_best_weights=True
+                )
+
+callbacks = [earlystop, ]
+
 
 cvae = ConVae(
         input_dim,
@@ -29,4 +44,5 @@ cvae = ConVae(
 
 cvae.CompileModel()
 cvae.CompileLoss()
-cvae.train()
+cvae.train(epochs=100, callbacks=callbacks)
+
---
+++ scripts/preprocess_real.py
@@ -0,0 +1,36 @@
+import numpy as np
+import h5py 
+from skimage import util
+
+which = "0210"
+
+with h5py.File("../data/proton-carbon-junk-noise-unlabeled-"+which+".h5", "r") as fo:
+    X_train = np.array(fo["images"])
+
+def rgb2gray(rgb):
+
+    r, g, b = rgb[:, :, :, 0], rgb[:, :, :, 1], rgb[:, :, :, 2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+    return gray
+
+
+X_train = rgb2gray(X_train)/255
+
+for i, x in enumerate(X_train):
+    X_train[i] = util.invert(x)
+
+threshhold_indices_train = X_train < 1e-2
+
+X_train[threshhold_indices_train] = 0
+
+# xStd = np.std(X_train[np.nonzero(X_train)])
+# X_train = X_train/xStd
+# X_test = X_test/xStd
+
+
+X_train = X_train.reshape(X_train.shape + (1,))
+
+t_fn = "../data/processed/all_"+which+".npy"
+
+np.save(t_fn, X_train)
