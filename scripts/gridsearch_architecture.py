4871aebef29277d7a34ca0355dbcce08fd2674d3
Robert Solli <solli.robert@gmail.com>
2019-05-08 23:17:57 +0200

    tidying commit

--- scripts/gridsearch_architecture.py
+++ scripts/gridsearch_architecture.py
@@ -6,6 +6,7 @@ import numpy as np
 import tensorflow as tf
 
 from sklearn.model_selection import  cross_val_score
+from sklearn.metrics import f1_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 
@@ -14,6 +15,8 @@ import h5py
 import os
 
 from draw import DRAW
+from hypperparam_generator import make_hyperparam
+
 from counter import *
 
 print("PID :", os.getpid())
@@ -32,35 +35,44 @@ def longform_latent(latent,):
     return longform_samples
 
 
-def compute_accuracy(X, y):
+def compute_accuracy(X, y, Xtest, ytest):
 
     model = LogisticRegression(
-            solver="liblinear",
-            multi_class="ovr",
-            class_weight="balanced"
+            penalty="l2",
+            solver="newton-cg",
+            multi_class="multinomial",
+            class_weight="balanced",
+            max_iter=1000,
         )
 
     mlp = MLPClassifier(
             max_iter=1000,
-            batch_size=10,
+            batch_size=100,
             hidden_layer_sizes=(80, 30, 10),
             early_stopping=True,
-            learning_rate_init=0.01
+            learning_rate_init=0.001
         )
 
-    mlp.fit(X, y)
     model.fit(X, y)
+    mlp.fit(X, y)
 
-    logreg_score = np.average(cross_val_score(model, X, y, cv=4))
-    mlp_score = np.average(cross_val_score(mlp, X, y, cv=4))
-    return logreg_score, mlp_score
+    train_score_lr = f1_score(y, model.predict(X), average=None)
+    test_score_lr = f1_score(ytest, model.predict(Xtest), average=None)
+    lr_scores = (train_score_lr, test_score_lr)
 
-T = 5
-delta = 0.8
-N = 85
-batch_size = 86
+    train_score_mlp = f1_score(y, mlp.predict(X), average=None)
+    test_score_mlp = f1_score(ytest, mlp.predict(Xtest), average=None)
+    mlp_scores = (train_score_mlp, test_score_mlp)
 
-repeats = 2
+    return lr_scores, mlp_scores
+
+
+delta = 0.98
+N = 65
+batch_size = 150
+enc_size = 500 
+
+num_simulations = 100
 epochs = 20
 
 read_N = N
@@ -69,152 +81,160 @@ write_N = N
 delta_write = delta
 delta_read = delta
 
-array_delta_w = np.zeros((batch_size, 1))
-array_delta_w.fill(delta_write)
-array_delta_w = array_delta_w.astype(np.float32)
-
-array_delta_r = np.zeros((batch_size, 1))
-array_delta_r.fill(delta_read)
-array_delta_r = array_delta_r.astype(np.float32)
-
 treshold_value = 0.4
 treshold_data = False
 
 all_0130 = np.load("../data/processed/all_0130.npy")
 train_data = np.load("../data/processed/train.npy")
+test_data = np.load("../data/processed/test.npy")
 
 with h5py.File("../data/images.h5", "r") as fo:
-    train_targets = np.array(fo["train_targets"])[:-1]
+    train_targets = np.array(fo["train_targets"])
+    test_targets = np.array(fo["test_targets"])
 
 #all_0210 = np.load("../data/processed/all_0210.npy")
 #all_data = np.concatenate([all_0130, all_0210])
 
 all_data = all_0130
 
+data_ind = np.arange(all_data.shape[0])
+chosen_in = np.random.choice(data_ind, 20000, replace=False)
+all_data = all_data[chosen_in]
+
 if treshold_data:
 	all_data[all_data < treshold_value] = 0
 
-state_sizes = np.arange(1000, 2000, 200)
-state_sizes = state_sizes[::-1]
+hyperparam_vals = [
+        "T, dec_state_size, latent_dim, loss_type, rw_type, \
+        n_enc, n_dec, beta", ]
 
-latent_sizes = np.arange(10, 20, 2)
-latent_sizes = latent_sizes[::-1]
+loss_record = np.zeros((num_simulations, 2, epochs))
+clf_record = np.zeros((num_simulations, 2, 2, 3))
 
-include_kl = [True, False]
+hp_gen = make_hyperparam()
 
-loss_record_shape = (
-        len(state_sizes),
-        len(latent_sizes),
-        len(include_kl),
-        repeats, 
-        2,
-        epochs
-        )
+for i in range(num_simulations):
 
-accuracy_record_shape = (
-        len(state_sizes),
-        len(latent_sizes),
-        len(include_kl),
-        repeats,
-        2
-        )
+    T, state_size, latent_dim, loss_type, rw_type, n_enc, n_dec, beta = hp_gen.generate_config()
+    hyperparam_vals.append([T, state_size, latent_dim, loss_type, rw_type, n_enc, n_dec, beta])
 
-loss_record = np.zeros(loss_record_shape)
-classification_recod = np.zeros(accuracy_record_shape)
+    use_attention = False
+    use_conv = False
 
-hyperparam_vals = np.array((
-        state_sizes,
-        latent_sizes,
-        include_kl,
-        repeats,
-        [0, 1],
-        epochs,
-        ))
+    if rw_type == "attention":
+        use_attention = True
+    if rw_type =="conv":
+        use_conv = True
 
-for i, state_size in enumerate(state_sizes):
+    use_kl = False
+    use_MMD = False
 
-    enc_size = state_size
+    if loss_type == "MMD":
+        use_MMD = True
+    elif loss_type == "KL":
+        use_kl = True
+
     dec_size = state_size
 
-    for j, latent_dim in enumerate(latent_sizes):
+    attn_config = {
+                "read_N": read_N,
+                "write_N": write_N,
+                "write_N_sq": write_N**2,
+                "delta_w": delta,
+                "delta_r": delta,
+            }
 
-        for k, use_kl in enumerate(include_kl):
+    if not use_attention:
+        attn_config = None
 
-            for l in range(repeats):
+    mode_config = {
+            "simulated_mode": False,
+            "restore_mode": False,
+            "include_KL": use_kl,
+            "include_MMD": use_MMD
+            }
 
-                attn_config = {
-                            "read_N": read_N,
-                            "write_N": write_N,
-                            "write_N_sq": write_N**2,
-                            "delta_w": array_delta_w,
-                            "delta_r": array_delta_r,
-                        }
+    draw_model = DRAW(
+            T,
+            dec_size,
+            enc_size,
+            latent_dim,
+            all_data,
+            beta=beta,
+            attn_config=attn_config,
+            mode_config=mode_config,
+            use_conv=use_conv,
+            use_attention=use_attention
+            )
 
-                mode_config = {
-                        "simulated_mode": False
-                        }
+    graph_kwds = {
+            "initializer": tf.initializers.glorot_normal,
+            "n_encoder_cells": n_enc,
+            "n_decoder_cells": n_dec,
+            }
 
-                draw_model = DRAW(
-                        T,
-                        dec_size,
-                        enc_size,
-                        latent_dim,
-                        batch_size,
-                        all_data,
-                        attn_config=attn_config,
-                        mode_config=mode_config
-                        )
+    loss_kwds = {
+            "reconst_loss": None,
+            }
 
-                graph_kwds = {
-                        "initializer": tf.initializers.glorot_normal
-                        }
+    draw_model.CompileModel(graph_kwds, loss_kwds)
 
-                loss_kwds = {
-                        "reconst_loss": None,
-                        "include_KL": use_kl
-                        }
+    opt = tf.train.AdamOptimizer
+    opt_args = [1e-3,]
+    opt_kwds = {
+            "beta1": 0.5,
+            }
 
-                draw_model.CompileModel(graph_kwds, loss_kwds)
+    draw_model.computeGradients(opt, opt_args, opt_kwds)
 
-                opt = tf.train.AdamOptimizer
-                opt_args = [1e-2,]
-                opt_kwds = {
-                        "beta1": 0.5,
-                        }
+    sess = tf.InteractiveSession()
 
-                draw_model.computeGradients(opt, opt_args, opt_kwds)
+    data_dir = "../drawing"
+    model_dir = "../models"
 
-                sess = tf.InteractiveSession()
+    lx, lz, = draw_model.train(
+                            sess,
+                            epochs,
+                            data_dir,
+                            model_dir,
+                            batch_size,
+                            save_checkpoints=False)
 
-                data_dir = "../drawing"
-                model_dir = "../models"
+    if any(np.isnan(lx)) or any(np.isnan(lz)):
+            continue
 
-                lx, lz, = draw_model.train(sess, epochs, data_dir, model_dir, save_checkpoints=False)
+    print()
+    loss_record[i][0] = lx
+    loss_record[i][1] = lz
 
-                if any(np.isnan(lx)) or any(np.isnan(lz)):
-                        continue
+    draw_model.X = train_data
 
-                print()
-                loss_record[i, j, k, l, 0] = lx
-                loss_record[i, j, k, l, 1] = lz
+    latent_values , _, _ = draw_model.generate_latent(sess, "../drawing", (train_data, test_data), save=False)
+    latent_train = longform_latent(latent_values[0])
+    latent_test = longform_latent(latent_values[1])
 
-                draw_model.X = train_data
+    lr_scores, mlp_scores = compute_accuracy(
+                                latent_train, train_targets,
+                                latent_test, test_targets
+                                )
 
-                latent_values , _, _ = draw_model.generateLatent(sess, "../drawing", (train_data, ), save=False)
-                latent_values = longform_latent(latent_values[0])
 
-                accuracies = compute_accuracy(latent_values, train_targets)
-                classification_recod[i, j, k, l] = accuracies
+    print("--------------------")
+    print("Dec size: ", dec_size, " Latent dim : ", latent_dim, " loss : ", loss_type)
+    print("rw_type: ", rw_type, " T :", T, "beta : ", beta )
+    print("####### TRAIN scores #######")
+    print("logreg_score", lr_scores[0], "mlp_scores", mlp_scores[0])
+    print("####### TEST scores #######")
+    print("logreg_score", lr_scores[1], "mlp_scores", mlp_scores[1])
+    print("---------------------")
 
-                print("--------------------")
-                print("Cell size: ", enc_size, " Latent dim : ", latent_dim, " KL : ", use_kl)
-                print("logreg_acc", accuracies[0], "mlp_acc", accuracies[1])
-                print("---------------------")
-                sess.close()
+    clf_record[i][0] = lr_scores
+    clf_record[i][1] = mlp_scores
+    sess.close()
 
-            np.save("../loss_records/delta_opt_{}.npy".format(RUN_COUNT) ,loss_record)
-            np.save("../loss_records/hyperparam_vals_{}.npy".format(RUN_COUNT),hyperparam_vals)
-            np.save("../loss_records/accuracy_vals{}.npy".format(RUN_COUNT), classification_recod)
+    np.save("../loss_records/architecture_opt{}.npy".format(RUN_COUNT) ,loss_record)
+    np.save("../loss_records/arch_hyperparam_vals_{}.npy".format(RUN_COUNT),hyperparam_vals)
+    np.save("../loss_records/arch_score_vals{}.npy".format(RUN_COUNT), clf_record)
 
 with open("counter.py", "w") as fo:
     fo.write("RUN_COUNT = {}".format(RUN_COUNT + 1))
