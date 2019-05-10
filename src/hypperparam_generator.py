1812065e258ebf22e1333a5baee53e55af6696ab
Robert Solli <solli.robert@gmail.com>
2019-05-10 20:05:24 +0200

    updated param search

--- src/hypperparam_generator.py
+++ src/hypperparam_generator.py
@@ -7,9 +7,9 @@ class make_hyperparam:
 
         self.T = np.arange(2, 44, 4)
 
-        self.dec_state_sizes = np.arange(100, 2500, 100)
+        self.dec_state_sizes = np.arange(100, 2000, 200)
         #self.dec_state_sizes = np.arange(2, 20, 10)
-        self.latent_sizes = np.arange(5, 150, 10)
+        self.latent_sizes = np.arange(5, 100, 10)
         #self.latent_sizes = np.arange(2, 4, 1)
 
         self.losses = ["MMD", "KL", ]
@@ -21,12 +21,22 @@ class make_hyperparam:
         self.betas = np.arange(0, 4)
         self.betas = 10**self.betas
 
+        self.etas = np.arange(-4, -1)
+        self.etas = np.power(10., self.etas)
+
+        self.epochs = np.arange(10, 110, 10)
+
+
     def generate_config(self,):
 
         T = np.random.choice(self.T)
-        dec_state_size = np.random.choice(self.dec_state_sizes)    
+
+        dec_state_size = np.random.choice(self.dec_state_sizes)
+
         latent_dim = np.random.choice(self.latent_sizes)
+
         loss = random.choice(self.losses)
+
         rw_method = random.choice(self.read_write)
 
         n_enc = np.random.choice(self.n_enc_cells)
@@ -34,4 +44,8 @@ class make_hyperparam:
 
         beta  = np.random.choice(self.betas)
 
-        return T, dec_state_size, latent_dim, loss, rw_method, n_enc, n_dec, beta
+        eta = np.random.choice(self.etas)
+
+        epoch = np.random.choice(self.epochs)
+
+        return T, dec_state_size, latent_dim, loss, rw_method, n_enc, n_dec, beta, eta, epoch
