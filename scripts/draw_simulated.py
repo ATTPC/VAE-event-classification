0fe72c1e39926819a3edbaa909907aa0f8f26875
Robert Solli <solli.robert@gmail.com>
2019-08-23 17:27:26 +0200

    updated to conform

--- scripts/draw_simulated.py
+++ scripts/draw_simulated.py
@@ -12,7 +12,9 @@ matplotlib.use("Agg")
 
 from draw import DRAW
 from counter import *
+import data_loader as dl
 
+train_data, test_data, test_labels = dl.load_simulated("128")
 
 T = 6
 enc_size = 400
@@ -21,9 +23,6 @@ latent_dim = 3
 epochs = 150
 
 batch_size = 50
-train_data = np.load("../data/simulated/pr_train_simulated.npy")
-test_data = np.load("../data/simulated/pr_test_simulated.npy")
-
 delta = 0.8
 N = 55
 
@@ -50,31 +49,47 @@ attn_config = {
 }
 
 
+mode_config = {
+    "simulated_mode":False, #deprecated, to be removed
+    "restore_mode":False, #indicates whether to load weights 
+    "include_KL":False, #whether to compute the KL loss over the latent space
+    "include_MMD":True, #same as above, but MMD 
+    "include_KM":False, #same as above, but K-means. See thesis for a more in-depth treatment of these
+    "batchnorm":False, #whether to include batch-normalization between layers
+    "use_vgg":False, #whether the input data is from a pre-trained model 
+    "use_dd":False, #whether to use the dueling-decoder objective 
+}
+
+
 draw_model = DRAW(
-    T, dec_size, enc_size, latent_dim, batch_size, train_data, attn_config=attn_config
+    T,
+    dec_size,
+    enc_size,
+    latent_dim,
+    train_data,
+    attn_config=attn_config,
+    mode_config=mode_config
 )
 
-graph_kwds = {"initializer": tf.initializers.glorot_normal}
+graph_kwds = {"initializer": tf.initializers.glorot_normal, }
+loss_kwds = {"reconst_loss": None,}
 
-loss_kwds = {"reconst_loss": None, "include_KL": False}
-
-draw_model.CompileModel(graph_kwds, loss_kwds)
+draw_model.compile_model(graph_kwds, loss_kwds)
 
 opt = tf.train.AdamOptimizer
 opt_args = [1e-2]
 opt_kwds = {"beta1": 0.5}
 
-draw_model.computeGradients(opt, opt_args, opt_kwds)
+draw_model.compute_gradients(opt, opt_args, opt_kwds)
 
 sess = tf.InteractiveSession()
 
 data_dir = "../drawing"
 model_dir = "../models"
 
-lx, lz, = draw_model.train(sess, epochs, data_dir, model_dir)
+lx, lz, = draw_model.train(sess, epochs, batch_size, data_dir, model_dir)
 
 draw_model.generateLatent(sess, "../drawing", (train_data, test_data))
-
 draw_model.generateSamples("../drawing", "../drawing")
 
 sess.close()
